326425393b36d243fad971509fc7123ce28c4669
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-07-07 21:41:08 -0700

    Installed react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { app_router } from "./routes";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={app_router} />
+  </StrictMode>
+);
---
+++ frontend/src/routes.ts
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+export const app_router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
