d44776ecdc5b70d430a7114b9aa658abb98531ef
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-07-08 20:42:12 -0700

    Added navigation bar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import SiteNavbar from "./SiteNavbar";
 
 function App() {
   return (
     <div>
+      <SiteNavbar />
       <h1>Welcome to Yucheng's Tutoring!</h1>
     </div>
   );
---
+++ frontend/src/SiteNavbar.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+
+function SiteNavbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Yucheng's Tutoring</Link>
+        </li>
+        <li>
+          <Link to="/">Book</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default SiteNavbar;
