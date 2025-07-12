b1bc9ae71f1f98694e62c8d7928e17c0199a223e
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-07-11 20:35:50 -0700

    Changed backend app setup
    Factored out get_available_appointments to separate file and changed app creation to a factory file for better project organization

---
+++ backend/app/book.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, request, jsonify
+from .database_setup import pool
+from psycopg.rows import dict_row
+
+bp = Blueprint("book", __name__, url_prefix="/api/book")
+
+
+@bp.route("/get_available_appointments")
+def get_available_appointments():
+    date = request.args.get("date")
+
+    with pool.connection() as conn:
+        cur = conn.cursor(row_factory=dict_row)
+
+        cur.execute(
+            """
+            SELECT ats.appointmentID AS appointment_id, ats.date AS date, ats.hour24 AS hour_24, ats.capacity AS capacity,
+                   COUNT(b.userID) AS slots_booked
+            FROM AppointmentTimeSlots ats
+            LEFT OUTER JOIN Bookings b
+                ON ats.appointmentID = b.appointmentID
+            WHERE ats.date = %(date)s
+            GROUP BY ats.appointmentID, ats.date, ats.hour24, ats.capacity
+            HAVING ats.capacity - COUNT(b.userID) > 0;
+            """,
+            {"date": date},
+        )
+
+        records = cur.fetchall()
+        appointments = []
+        for record in records:
+            appointments.append(
+                {
+                    "appointment_id": record.get("appointment_id"),
+                    "date": str(record.get("date")),
+                    "hour_24": record.get("hour_24"),
+                    "capacity": record.get("capacity"),
+                    "slots_booked": record.get("slots_booked"),
+                }
+            )
+
+        return jsonify({"success": True, "appointments": appointments})
+
+    return jsonify({"success": False})
