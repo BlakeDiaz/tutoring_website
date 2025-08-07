b44a02814657052d6f80a7ecdd25b92b8a2fbbcd
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-08-07 15:26:42 -0700

    Changed configuration to use classes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,33 +1,18 @@
 from flask import Flask
-from .config import config
+from .config import Config, DevelopmentConfig
 
 
-def create_app():
+def create_app(config: Config = DevelopmentConfig()):
     app = Flask(__name__)
-
-    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
-    # TODO set to true in production
-    app.config["JWT_COOKIE_SECURE"] = False
-    app.config["JWT_SECRET_KEY"] = config["credentials.flask"]["jwt_secret_key"]
-    app.config["JWT_CSRF_METHODS"] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    app.config.from_object(config)
 
     from .jwt import jwt
-
-    jwt.init_app(app)
-
-    db_config = config["credentials.database"]
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        f"postgresql+psycopg://{db_config["user"]}:{db_config["password"]}"
-        + f"@{db_config["host"]}:{db_config["port"]}/{db_config["name"]}"
-    )
-
     from .db import db
-
-    db.init_app(app)
-
     from . import book
     from . import auth
 
+    jwt.init_app(app)
+    db.init_app(app)
     app.register_blueprint(book.bp)
     app.register_blueprint(auth.bp)
 
--- backend/app/config.py
+++ backend/app/config.py
@@ -4,3 +4,33 @@ import os
 dirname = os.path.dirname(__file__)
 config = ConfigParser()
 config.read(os.path.join(dirname, "../config.ini"))
+
+
+class Config:
+    TESTING = False
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = False
+    JWT_SECRET_KEY = config["credentials.flask"]["jwt_secret_key"]
+    JWT_CSRF_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    DB_USER = config["credentials.database"]["user"]
+    DB_PASSWORD = config["credentials.database"]["password"]
+    DB_HOST = config["credentials.database"]["host"]
+    DB_PORT = config["credentials.database"]["port"]
+    DB_NAME = config["credentials.database"]["name"]
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class ProductionConfig(Config):
+    JWT_COOKIE_SECURE = True
+
+
+class DevelopmentConfig(Config):
+    pass
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DB_NAME = config["credentials.test_database"]["name"]
