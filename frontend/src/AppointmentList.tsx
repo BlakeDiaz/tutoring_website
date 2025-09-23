b116bd29428edf6bdc7f6cdf76f23c3c18a37316
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-09-23 11:40:43 -0700

    Added style to AppointmentList

--- frontend/src/AppointmentList.tsx
+++ frontend/src/AppointmentList.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router";
-import { type Date, formatHour24ToHour12 } from "./dates";
+import { type Date, formatDateForAppointmentSmall, formatHour24ToHour12 } from "./dates";
 import { type JSX } from "react";
 import { type Appointment } from "./appointments";
-import { formatDateForAppointment } from "./dates";
+import "./AppointmentList.css";
 
 function AppointmentList(props: { date: Date; appointments: Appointment[] }) {
-  const date_formatted = formatDateForAppointment(props.date);
+  const date_formatted = formatDateForAppointmentSmall(props.date);
 
   return (
-    <div>
-      <h2>{date_formatted}</h2>
+    <div className="appointment-list">
+      <h1 className="appointment-list-header">{date_formatted}</h1>
       {renderAppointmentBookingLinks(props.appointments)}
     </div>
   );
@@ -21,36 +21,23 @@ const renderAppointmentBookingLinks = (appointments: Appointment[]): JSX.Element
     const hour_24 = appointment.hour_24;
     const hour_formatted = formatHour24ToHour12(hour_24);
 
-    if (appointment.slots_booked === 0) {
-      links.push(
-        <li key={appointment.appointment_id}>
-          <Link
-            to={{
-              pathname: "/book_new_appointment",
-              search: `appointment_id=${encodeURIComponent(appointment.appointment_id)}`,
-            }}
-          >
-            {hour_formatted}
-          </Link>
-        </li>
-      );
-    } else {
-      links.push(
-        <li key={appointment.appointment_id}>
-          <Link
-            to={{
-              pathname: "/book_existing_appointment",
-              search: `appointment_id=${encodeURIComponent(appointment.appointment_id)}`,
-            }}
-          >
-            {hour_formatted}
-          </Link>
-        </li>
-      );
-    }
+    const pathname = appointment.slots_booked === 0 ? "/book_new_appointment" : "/book_existing_appointment";
+    links.push(
+      <li key={appointment.appointment_id}>
+        <Link
+          className="appointment-list-link"
+          to={{
+            pathname: pathname,
+            search: `appointment_id=${encodeURIComponent(appointment.appointment_id)}`,
+          }}
+        >
+          {hour_formatted} - {appointment.capacity - appointment.slots_booked}/{appointment.capacity} slots available
+        </Link>
+      </li>
+    );
   }
 
-  return <ul>{links}</ul>;
+  return <ul className="appointment-list-links">{links}</ul>;
 };
 
 export default AppointmentList;
