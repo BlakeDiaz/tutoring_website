dcfb77ceaee8daad75bcff7b6741dddbc8de0cca
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-09-27 14:50:29 -0700

    Added CSS to AdminNavbar

--- frontend/src/AdminNavbar.tsx
+++ frontend/src/AdminNavbar.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router";
+import "./SiteNavbar.css";
+import { useState } from "react";
 
 function AdminNavbar() {
+  if (window.innerWidth >= 1200) {
+    return <AdminNavbarDesktop></AdminNavbarDesktop>;
+  } else {
+    return <AdminNavbarMobile></AdminNavbarMobile>;
+  }
+}
+
+function AdminNavbarDesktop() {
   return (
-    <nav>
+    <nav className="navbar-desktop">
       <ul>
         <li>
-          <Link to="/admin">Admin</Link>
+          <Link className="logo-link" to="/admin">
+            Admin
+          </Link>
         </li>
         <li>
           <Link to="/admin_appointment_panel">Appointment Panel</Link>
@@ -15,6 +27,35 @@ function AdminNavbar() {
         </li>
       </ul>
     </nav>
+  );
+}
+
+function AdminNavbarMobile() {
+  const [show_links, setShowLinks] = useState(false);
+
+  function renderSubLinks() {
+    if (show_links) {
+      return (
+        <div>
+          <Link to="/admin_appointment_panel">Appointment Panel</Link>
+          <Link to="/admin_add_appointment">Add Appointment</Link>
+        </div>
+      );
+    }
+
+    return <></>;
+  }
+
+  return (
+    <div className="navbar-mobile">
+      <Link className="logo-link" to="/admin">
+        Admin
+      </Link>
+      {renderSubLinks()}
+      <a className="icon" onClick={() => setShowLinks(!show_links)}>
+        <i className="fa fa-bars"></i>
+      </a>
+    </div>
   );
 }
 
