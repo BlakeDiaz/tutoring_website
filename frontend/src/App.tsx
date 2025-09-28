25e898d3689d453a699b74ff008797f8959ec7b4
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-09-28 11:36:51 -0700

    Added CSS and content to homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,31 @@
+import { Link } from "react-router";
 import "./App.css";
 import SiteNavbar from "./SiteNavbar";
+import study_image from "./assets/study_image.jpg";
 
 function App() {
   return (
     <div>
       <SiteNavbar />
-      <h1>Welcome to Yucheng's Tutoring!</h1>
+      <div className="homepage-wrapper">
+        <div className="homepage-text">
+          <h1 className="homepage-header">Learn. Understand. Succeed.</h1>
+          <p className="homepage-paragraph">
+            Take your next step towards succees at Yucheng's Tutoring. We offer personalized tutoring sessions in a wide
+            array of subjects, from Calculus to Organic Chemistry. Invest in your future today, an reap the rewards
+            tomorrow.
+          </p>
+          <div className="links-wrapper">
+            <Link className="button-link" to="/book">
+              Book Today
+            </Link>
+            <Link className="redirect-link" to="/register">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+        <img className="homepage-image" src={study_image}></img>
+      </div>
     </div>
   );
 }
