b035be18b223b4853d408e8c5ab5740fb3631923
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2025-12-28 23:35:31 -0800

    Updated configuration files to work with docker

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ import os
 
 dirname = os.path.dirname(__file__)
 config = ConfigParser()
-config.read(os.path.join(dirname, "../config.ini"))
+config.read("/run/secrets/backend_config")
 
 
 class Config:
